14abf9b4214443dc4ae51f25514cdea63e5d6429
FrederoxDev <blakefreddie96@gmail.com>
2023-12-08 20:10:27 +0000

    item identifier:namespace

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <stdint.h>
 #include <iostream>
 #include <fstream>
@@ -21,6 +21,13 @@ static void Item_appendFormattedHovertext(Item* self, const ItemStackBase& itemS
         uint64_t current = max - itemStack.mItem->getDamageValue(itemStack.mUserData);
         text.append(fmt::format("\nDurability: {} / {}", current, max));
     }
+
+    auto itemNamespace = reinterpret_cast<std::string*>(reinterpret_cast<uint64_t>(self) + 248);
+
+    std::string rawNameId;
+    itemStack.getRawNameId(rawNameId);
+    text.append(fmt::format("\n{}8{}:{}", "\xc2\xa7", *itemNamespace, rawNameId));
+
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
@@ -34,6 +41,8 @@ extern "C" __declspec(dllexport) void Initialize() {
         SigScan("40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 49 8B F1 4C 89 44 24 ? 4C 8B F2 48 8B D9"),
         &Item_appendFormattedHovertext, reinterpret_cast<void**>(&_Item_appendFormattedHovertext)
     );
+
+    Log::Info("Sizeof: {}\n", sizeof(std::string));
 }
 
 extern "C" __declspec(dllexport) void Shutdown() {
