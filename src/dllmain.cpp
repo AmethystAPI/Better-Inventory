a8851ee5e6236a4124ba6f0b72170b31b2ab9870
FrederoxDev <blakefreddie96@gmail.com>
2023-12-14 17:53:36 +0000

    Add display for item numerical id, fix color formatting which call super.appendFormattedHoverText by resetting text effects

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -8,6 +8,7 @@
 #include "MinHook.h"
 
 #include "minecraft/src/common/world/item/Item.h"
+#include "minecraft/src-client/common/client/renderer/screen/MinecraftUIRenderContext.h"
 
 HookManager hookManager;
 
@@ -15,19 +16,18 @@ Item::_appendFormattedHovertext _Item_appendFormattedHovertext;
 
 static void Item_appendFormattedHovertext(Item* self, const ItemStackBase& itemStack, Level& level, std::string& text, uint8_t a5) {
     _Item_appendFormattedHovertext(self, itemStack, level, text, a5);
-    uint64_t max = itemStack.mItem->getMaxDamage();
+    Item* item = itemStack.mItem;
+
+    uint64_t max = item->getMaxDamage();
 
     if (max != 0) {
-        uint64_t current = max - itemStack.mItem->getDamageValue(itemStack.mUserData);
+        uint64_t current = max - item->getDamageValue(itemStack.mUserData);
         text.append(fmt::format("\nDurability: {} / {}", current, max));
     }
 
-    auto itemNamespace = reinterpret_cast<std::string*>(reinterpret_cast<uint64_t>(self) + 248);
-
     std::string rawNameId;
     itemStack.getRawNameId(rawNameId);
-    text.append(fmt::format("\n{}8{}:{}", "\xc2\xa7", *itemNamespace, rawNameId));
-
+    text.append(fmt::format("\n{}8{}:{} ({}){}r", "\xc2\xa7", item->mNamespace, rawNameId, item->mId, "\xc2\xa7"));
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
@@ -41,8 +41,6 @@ extern "C" __declspec(dllexport) void Initialize() {
         SigScan("40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 49 8B F1 4C 89 44 24 ? 4C 8B F2 48 8B D9"),
         &Item_appendFormattedHovertext, reinterpret_cast<void**>(&_Item_appendFormattedHovertext)
     );
-
-    Log::Info("Sizeof: {}\n", sizeof(std::string));
 }
 
 extern "C" __declspec(dllexport) void Shutdown() {
